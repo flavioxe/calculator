34eb27eadd15a1f7466174daf667c2e577511846
Flávio Mendonça <flaviomhenrique@gmail.com>
2024-03-09 13:55:08 -0300

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import { useState } from "react";
 import { Caulculator } from "./components/Calculator";
 
 import styles from "./App.module.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <section
         className={`${styles.wrapper} container d-flex flex-column align-items-center justify-content-center`}
       >
-        <h1 className={styles.title}>Simple Caulculator</h1>
+        <h1 className={styles.title}>Simple Calculator</h1>
         <Caulculator />
-
-        <div className="">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
       </section>
     </>
   );
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,21 +1,81 @@
+import { useState } from "react";
 import styles from "./Calculator.module.scss";
 
 export function Caulculator() {
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function handleChangeFirstNumber(e) {
+    setFirstNumber(Number(e.target.value));
+  }
+
+  function handleChangeSecondNumber(e) {
+    setSecondNumber(Number(e.target.value));
+  }
+
   return (
     <>
       <section className={`${styles.inputWrapper} d-flex align-items-center`}>
-        <aside>
-          <p className={styles.inputText}>Type the first number:</p>
-          <input type="number" placeholder="Type here" />
+        <aside className="d-flex flex-column">
+          <label className={styles.inputText}>Type the first number:</label>
+          <input
+            type="number"
+            placeholder="Type here"
+            value={firstNumber}
+            onChange={handleChangeFirstNumber}
+          />
         </aside>
 
-        <aside>
-          <p className={styles.inputText}>Type the second number:</p>
-          <input type="number" placeholder="Type here" />
+        <aside className="d-flex flex-column">
+          <label className={styles.inputText}>Type the second number:</label>
+          <input
+            type="number"
+            placeholder="Type here"
+            value={secondNumber}
+            onChange={handleChangeSecondNumber}
+          />
         </aside>
       </section>
 
-      <h4 className={styles.resultText}>The result is:</h4>
+      <section
+        className={`${styles.operatorsWrapper} d-flex flex-column align-items-center`}
+      >
+        <p className={styles.inputText}>Choose the operation:</p>
+
+        <main className={`${styles.buttonsWrapper} d-flex align-items-center`}>
+          <button
+            onClick={() => {
+              setResult(firstNumber + secondNumber);
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              setResult(firstNumber - secondNumber);
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={() => {
+              setResult(firstNumber * secondNumber);
+            }}
+          >
+            *
+          </button>
+          <button
+            onClick={() => {
+              setResult(firstNumber / secondNumber);
+            }}
+          >
+            /
+          </button>
+        </main>
+      </section>
+
+      <h4 className={styles.resultText}>The result is: {result}</h4>
     </>
   );
 }
